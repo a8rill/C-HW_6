b922e3cd70463150ae570b06e7bc555d62eaf70c
Olua <a8rill@mail.ru>
2023-03-09 20:59:43 +0300

    added binary_number project

---
+++ binary_number/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 42 (необязательно): Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+Console.WriteLine("Введите десятичное число:");
+int number = int.Parse(Console.ReadLine());
+
+var binary = Convert.ToString(number,2);
+Console.WriteLine($"Число {number} в двоичной  систему счисления: {binary}");
